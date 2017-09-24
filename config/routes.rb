19b66d5e972dfedf0e6d1f7d9ddd04c4f7f89d9a
Kevin Kawai <kevin.fredricks@gmail.com>
2017-09-24 05:00:23 +0000

    added links to login and signup on front page

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "static_pages#index"
   get 'login', to: 'sessions#new', as: 'login'
-  get 'sessions', to: 'sessions#destroy', as: 'logout'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'signup', to: 'users#new', as: 'signup'
   resources :users, only: [:new,:create]
   resources :sessions, only: :create
 end
