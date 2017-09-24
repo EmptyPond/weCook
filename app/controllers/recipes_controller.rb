10edce1ebf0ed00be6ea3e0296b071f3b1b0708e
Kevin Kawai <kevin.fredricks@gmail.com>
2017-09-24 14:29:26 +0000

    finished hooking up initial recipe pages, no edit

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,14 +1,18 @@
 class RecipesController < ApplicationController
   def index
+    @recipe = Recipe.all
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def show
+    @recipe = Recipe.find(params[:id])
   end
 
   def create
+    #since we don't have auth for this action right now will break if we do this while not logged in
     @recipe = current_user.recipes.create(recipe_params)
     if @recipe.save
       redirect_to recipe_path(id:@recipe.id)
