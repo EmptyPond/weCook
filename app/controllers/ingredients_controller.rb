46948462248eced6416854fe01949a3d1794ad8b
Kevin Kawai <kevin.fredricks@gmail.com>
2017-09-25 03:08:20 +0000

    resolved tests tests for new page

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,20 @@
 class IngredientsController < ApplicationController
+  before_action :require_login, only: :new
   def new
+    #probably needs to be refactored due to being long
+    #is this actually how I'm suppose to add ingredeints to recipe? feels smelly
+    @ingredient = Recipe.find(params[:recipe_id]).ingredients.new
+    #this will need to change because later multiple people will be able to follow a recipe
+    #also do we want an "owner" of a recipe?
+    if current_user != @ingredient.recipe.users.last
+      #should add alert here telling user that they can't edit this
+      redirect_to recipe_path(params[:recipe_id]), status: :forbidden
+    end
+  end
+
+  private
+
+  def ingredients_params
+    params.require(:ingredients).permit(:name,:amount)
   end
 end
--- spec/controllers/ingredients_controller_spec.rb
+++ spec/controllers/ingredients_controller_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe IngredientsController, type: :controller do
     it "should allow me to view new step page if I am logged in and own the recipe" do
       kit = FactoryGirl.create(:kitchen)
       login_user(kit.user)
-      get :new
+      get :new, params: {recipe_id: kit.recipe.id}
 
       expect(response).to have_http_status(:success)
     end
 
     it "shouldn't allow me to view new step page if I am not logged in" do
       kit = FactoryGirl.create(:kitchen)
-      get :new
+      get :new, params: {recipe_id: kit.recipe.id}
 
       expect(response).to redirect_to login_path
     end
@@ -21,7 +21,7 @@ RSpec.describe IngredientsController, type: :controller do
       recipe = FactoryGirl.create(:recipe)
       user = FactoryGirl.create(:user)
       login_user(user)
-      get :new
+      get :new, params: {recipe_id: recipe.id}
 
       expect(response).to have_http_status(:forbidden)
     end
