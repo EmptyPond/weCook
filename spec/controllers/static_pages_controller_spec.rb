f2993ce91b9c2891a80961a73a153f23a62fef96
Kevin Kawai <kevin.fredricks@gmail.com>
2017-09-29 01:24:48 +0000

    changed my_page to not need :id and use current_user instead

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -11,14 +11,14 @@ RSpec.describe StaticPagesController, type: :controller do
     it "should let you see my_page if you are logged in" do
       kitchen = FactoryGirl.create(:kitchen)
       login_user(kitchen.user.last)
-      get :my_page, params: { id: kitchen.user.last.id }
+      get :my_page
 
       expect(response).to have_http_status(:success)
     end
 
     it "should NOT let you see my_page if you are NOT logged in" do
       kitchen = FactoryGirl.create(:kitchen)
-      get :my_page, params: { id: kitchen.user.last.id }
+      get :my_page
 
       expect(response).to redirect_to login_path
     end
